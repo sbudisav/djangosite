10a2a1c6dcc02113ed544382e31f54e016400f45
sbudisav <sbudisav@gmail.com>
2020-04-30 15:46:54 -0500

    adds testing for polls

--- polls/models.py
+++ polls/models.py
@@ -11,6 +11,10 @@ class Question(models.Model):
 
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    #  POST TEST CORRECTION
+    # def was_published_recently(self):
+    #     now = timezone.now()
+    #     return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 
 class Choice(models.Model):
