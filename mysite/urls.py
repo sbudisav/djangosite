a27ff0fa3d1be76dfbbd03bdc11f8621cabb83f4
sbudisav <sbudisav@gmail.com>
2020-05-02 21:21:02 -0500

    adds login screen

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 from django.contrib.auth import views
 
 urlpatterns = [
-    # path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('accounts/', include('django.contrib.auth.urls')),
     path('polls/', include('polls.urls')),
 ]
