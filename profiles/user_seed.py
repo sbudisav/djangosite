911b544e0369547cbadd141f7af17ed8a20027d6
sbudisav <sbudisav@gmail.com>
2020-08-24 14:08:48 -0500

    seeds with dummy user data

--- profiles/user_seed.py
+++ profiles/user_seed.py
@@ -1,5 +1,10 @@
 from django_seed import Seed 
-
+from .models import UserProfile
 seeder = Seed.seeder()
 
-from .models import UserProfile, UserPlant, FollowedUser
+
+seeder.add_entity(UserProfile, 5, {
+  'username': lambda x: random.faker.name(),
+  'password': 'nomiss11',
+})
+seeder.execute()
